9cc18e68e79a15dcaeab861bf6411368a06d6cda
lowtiergod <88077988+lowtiergod@users.noreply.github.com>
2022-02-08 07:07:38 -0800

    fight function & ask for player robot name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! Javascript is running!");
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+
+var playerName = window.prompt("What is your robot's name?");
+window.alert(playerName);
+
+//fight();
